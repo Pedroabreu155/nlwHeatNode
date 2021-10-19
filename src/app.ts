3fe25dd95857763f99714886e28fa9f536aa88f2
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-10-19 14:25:32 -0300

    added socket.io

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,25 @@
 import 'dotenv/config';
 import express from 'express';
-
+import http from 'http';
+import cors from 'cors';
+import { Server } from 'socket.io';
 import { router } from './routes';
 
 const app = express();
 
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', socket => {
+  console.log(`User conectado no socket ${socket.id}`);
+});
+
+app.use(cors());
 app.use(express.json());
-
 app.use(router);
-
-app.get('/github', (request, response) => {
-  response.redirect(
-    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`,
-  );
-});
-
-app.get('/signin/callback', (request, response) => {
-  const { code } = request.query;
-
-  return response.json(code);
-});
 
 app.listen(4000, () => console.log(`App is running on port 4000 🔥`));
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateMessageController } from './controllers/CreateMessageController';
@@ -7,7 +7,20 @@ import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
 
 const router = Router();
 
+router.get('/github', (request: Request, response: Response) => {
+  response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`,
+  );
+});
+
+router.get('/signin/callback', (request, response) => {
+  const { code } = request.query;
+
+  return response.json(code);
+});
+
 router.post('/authenticate', new AuthenticateUserController().handle);
+
 router.post(
   '/messages',
   ensureAuthenticated,
