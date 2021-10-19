fc310a35303996e04d08d43b05879b6e68790b9b
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-10-18 23:25:59 -0300

    authenticating user

--- src/controllers/AuthenticateUserController.ts
+++ src/controllers/AuthenticateUserController.ts
@@ -6,9 +6,13 @@ class AuthenticateUserController {
     const { code } = request.body;
 
     const service = new AuthenticateUserService();
-    const result = await service.execute(code);
 
-    return response.json(result);
+    try {
+      const result = await service.execute(code);
+      return response.json(result);
+    } catch (error) {
+      return response.json({ error: error.message });
+    }
   }
 }
 
